5f274337ad7c47b0d8ae44df472a007443d2fa37
sandy <18382255942@163.com>
2022-06-20 14:30:04 +0800

    feat(): add everpay sdk

---
+++ config/tokenPrice_test.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_GetTokenPriceByRedstone(t *testing.T) {
+	price, err := GetTokenPriceByRedstone("VRT", "AR")
+	assert.NoError(t, err)
+	t.Log(price)
+	price, err = GetTokenPriceByRedstone("VRT", "ETH")
+	assert.NoError(t, err)
+	t.Log(price)
+
+	price, err = GetTokenPriceByRedstone("xyz", "AR")
+	assert.NoError(t, err)
+	t.Log(price)
+	price, err = GetTokenPriceByRedstone("ardrive", "AR")
+	assert.NoError(t, err)
+	t.Log(price)
+	price, err = GetTokenPriceByRedstone("pia", "AR")
+	assert.NoError(t, err)
+	t.Log(price)
+	price, err = GetTokenPriceByRedstone("dlt", "AR")
+	assert.NoError(t, err)
+	t.Log(price)
+}
